66734c3d9de47090927c55708bb320e56c013496
divi <ukattahdivine603@gmail.com>
2025-06-04 14:25:32 +0100

    chore: Responsiveness complete

---
+++ script.js
@@ -0,0 +1,19 @@
+const dropdown = document.getElementById("customDropdown");
+dropdown.addEventListener("click", () => {
+  dropdown.classList.toggle("open");
+});
+
+window.addEventListener("click", function (e) {
+  if (!dropdown.contains(e.target)) {
+    dropdown.classList.remove("open");
+  }
+});
+
+var counter = 1;
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+  if (counter > 5) {
+    counter = 1;
+  }
+}, 4000);
